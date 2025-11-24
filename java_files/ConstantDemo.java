42b1ac035c76ba70020d500f13cd7d6523fb6aaf
shkthuba <shkthuba3142@gmail.com>
2025-11-24 13:26:36 +0530

    added 2nd prog

---
+++ java_files/ConstantDemo.java
@@ -0,0 +1,9 @@
+class ConstantDemo {
+public static void main(String[] args) {
+final double PI = 3.14159; // constant
+int radius = 5;
+double area = PI * radius * radius;
+System.out.println("Radius: " + radius);
+System.out.println("Area of Circle: " + area);
+}
+}
