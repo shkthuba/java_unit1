bad1b47b1dea34b368bbca3e331bc00e5c2130d5
shkthuba <shkthuba3142@gmail.com>
2025-11-24 12:53:45 +0530

    added java porgram

---
+++ java_files/VariableDemo.java
@@ -0,0 +1,13 @@
+class VariableDemo {
+public static void main(String[] args) {
+int age = 25; // integer variable
+double salary = 55000.75; // double variable
+char grade = 'A'; // character variable
+boolean isStudent = false; // boolean variable
+
+System.out.println("Age: " + age);
+System.out.println("Salary: " + salary);
+System.out.println("Grade: " + grade);
+System.out.println("Is Student: " + isStudent);
+}
+}
